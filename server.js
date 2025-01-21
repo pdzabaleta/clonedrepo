77f7d3d2ba8c0e1abc4d86c0bf7e096cd6f4baea
pdzabaleta <pdzabaleta@gmail.com>
2025-01-20 23:38:30 -0300

    update

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+// added in w03
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -41,6 +43,4 @@ app.listen(port, () => {
 })
 
 // index rute
-app.get("/", function(req, res){
-  res.render("index", {title: "home"})
-})
+app.get("/", baseController.buildHome)
